0da6d76876de383c2d44db5a7363944c4859f297
Bao Nguyen <bao.nguyen@smoovapp.com>
2017-11-28 14:46:00 +0700

    add folder struct

---
+++ rxSwiftDemo/Example/SubjectExample.swift
@@ -0,0 +1,15 @@
+//
+//  SubjectExample.swift
+//  rxSwiftDemo
+//
+//  Created by Bao Nguyen on 11/28/17.
+//  Copyright © 2017 Bao Nguyen. All rights reserved.
+//
+
+import RxSwift
+
+struct SubjectExample {
+    static func running() {
+        
+    }
+}
