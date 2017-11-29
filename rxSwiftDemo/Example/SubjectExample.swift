998677285abf6eabba5bf873545ff8d2b35cd68e
Bao Nguyen <bao.nguyen@smoovapp.com>
2017-11-29 17:03:11 +0700

    completed challege

--- rxSwiftDemo/Example/SubjectExample.swift
+++ rxSwiftDemo/Example/SubjectExample.swift
@@ -117,5 +117,44 @@ struct SubjectExample {
             variable.value = "2"
         }
         
+        example(description: "PublishSubject") {
+            
+            let disposeBag = DisposeBag()
+            
+            let dealtHand = PublishSubject<[(String, Int)]>()
+            
+            func deal(_ cardCount: UInt) {
+                var deck = cards
+                var cardsRemaining: UInt32 = 52
+                var hand = [(String, Int)]()
+                
+                for _ in 0..<cardCount {
+                    let randomIndex = Int(arc4random_uniform(cardsRemaining))
+                    hand.append(deck[randomIndex])
+                    deck.remove(at: randomIndex)
+                    cardsRemaining -= 1
+                }
+                
+                // Add code to update dealtHand here
+                if points(for: hand) > 21 {
+                    dealtHand.onError(HandError.busted)
+                } else {
+                    dealtHand.onNext(hand)
+                }
+            }
+            
+            // Add subscription to dealtHand here
+            dealtHand
+                .subscribe(onNext: { (element) in
+                    print("Card string", cardString(for: element))
+                    print("Point", points(for: element))
+                }, onError: { (error) in
+                    print("Error")
+                })
+                .disposed(by: disposeBag)
+            
+            deal(3)
+        }
+        
     }
 }
