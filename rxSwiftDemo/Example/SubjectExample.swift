e47886b65b28a8efb18a685070f92f4cdd74d0f1
Bao Nguyen <bao.nguyen@smoovapp.com>
2017-11-29 16:36:16 +0700

    Add subject

--- rxSwiftDemo/Example/SubjectExample.swift
+++ rxSwiftDemo/Example/SubjectExample.swift
@@ -10,6 +10,112 @@ import RxSwift
 
 struct SubjectExample {
     static func running() {
+        example(description: "PublishSubject") {
+            let subject = PublishSubject<String>()
+            subject.onNext("Is anyone listening?")
+            let subscriptionOne = subject
+                .subscribe(onNext: { (element) in
+                    print(element)
+                })
+            subject.on(.next("1"))
+            subject.on(.next("2"))
+            
+            let subscriptionTwo = subject
+                .subscribe({ (event) in
+                    print("2)", event.element ?? event)
+                })
+            subject.onNext("3")
+            subscriptionOne.dispose()
+            subject.onNext("4")
+            
+            // 1
+            subject.onCompleted()
+            // 2
+            subject.onNext("5")
+            // 3
+            subscriptionTwo.dispose()
+            let disposeBag = DisposeBag()
+            // 4
+            subject
+                .subscribe {
+                    print("3)", $0.element ?? $0)
+                }
+                .disposed(by: disposeBag)
+            subject.onNext("?")
+        }
+        
+        enum MyError: Error {
+            case anError
+        }
+        example(description: "BehaviorSubject") {
+            
+            let subject = BehaviorSubject(value: "Initial value")
+            let disposeBag = DisposeBag()
+            subject
+                .subscribe({ (event) in
+                    print("1)", event.element ?? event.error ?? event)
+                })
+                .disposed(by: disposeBag)
+            subject.onNext("X")
+            subject.onError(MyError.anError)
+            
+            subject
+                .subscribe({ (event) in
+                    print("2)", event.element ?? event.error ?? event)
+                })
+                .disposed(by: disposeBag)
+        }
+        
+        example(description: "ReplaySubject") {
+            let subject = ReplaySubject<String>.create(bufferSize: 2)
+            let disposeBag = DisposeBag()
+            subject.onNext("1")
+            subject.onNext("2")
+            subject.onNext("3")
+            
+            subject
+                .subscribe({ (event) in
+                    print("1)", event.element ?? event.error ?? event)
+                })
+                .disposed(by: disposeBag)
+            
+            subject
+                .subscribe({ (event) in
+                    print("2)", event.element ?? event.error ?? event)
+                })
+                .disposed(by: disposeBag)
+            
+            subject.onNext("4")
+            subject
+                .subscribe({ (event) in
+                    print("3)", event.element ?? event.error ?? event)
+                })
+                .disposed(by: disposeBag)
+            
+            subject.onError(MyError.anError)
+            subject.dispose()
+        }
+        
+        example(description: "Variable") {
+            let variable = Variable<String>("Initial value")
+            let disposeBag = DisposeBag()
+            variable.value = "New initial value"
+            variable.asObservable()
+                .subscribe({ (event) in
+                    print("1)", event.element ?? event.error ?? event)
+                })
+                .disposed(by: disposeBag)
+            
+            variable.value = "1"
+            
+            variable.asObservable()
+                .subscribe({ (event) in
+                    print("2)", event.element ?? event.error ?? event)
+                })
+                .disposed(by: disposeBag)
+            
+            variable.value = "2"
+        }
         
     }
 }
