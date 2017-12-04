76187969e0c94a8ee6ffae51ee4fcf8ee81e8b9d
Bao Nguyen <bao.nguyen@smoovapp.com>
2017-12-04 12:02:09 +0700

    complete transforming operator

--- rxSwiftDemo/Example/TransformingOperators/TransformingElements.swift
+++ rxSwiftDemo/Example/TransformingOperators/TransformingElements.swift
@@ -26,6 +26,64 @@ struct TransformingElements {
             let disposeBag = DisposeBag()
             let formatter = NumberFormatter()
             formatter.numberStyle = .spellOut
+            Observable<NSNumber>.of(123, 4, 56)
+                .map {
+                    formatter.string(from: $0) ?? ""
+                }
+                .subscribe(onNext: {
+                    print($0)
+                })
+                .disposed(by: disposeBag)
+        }
+        
+        struct Student {
+            var score: Variable<Int>
+        }
+        
+        example(description: "flatMap") {
+            let disposebag = DisposeBag()
+            
+            let ryan = Student(score: Variable(80))
+            let charlotte = Student(score: Variable(90))
+            
+            let student = PublishSubject<Student>()
+            student.asObserver()
+                .flatMap {
+                    $0.score.asObservable()
+                }
+                .subscribe(onNext: {
+                    print($0)
+                })
+            .disposed(by: disposebag)
+            
+            student.onNext(ryan)
+            ryan.score.value = 10
+            student.onNext(charlotte)
+            ryan.score.value = 10
+            charlotte.score.value = 50
+        }
+        
+        example(description: "flatMapLatest") {
+            let disposebag = DisposeBag()
+            
+            let ryan = Student(score: Variable(80))
+            let charlotte = Student(score: Variable(90))
+            
+            let student = PublishSubject<Student>()
+            student.asObserver()
+                .flatMapLatest {
+                    $0.score.asObservable()
+                }
+                .subscribe(onNext: {
+                    print($0)
+                })
+                .disposed(by: disposebag)
+            
+            student.onNext(ryan)
+            ryan.score.value = 10
+            student.onNext(charlotte)
+            ryan.score.value = 10
+            charlotte.score.value = 50
         }
     }
 }
